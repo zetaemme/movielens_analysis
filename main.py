187e10b0ea79560d992aa1b7362aa9f5a2e7c736
zetaemme <mattia.zorzan@gmail.com>
2021-04-23 17:38:24 +0200

    Data plot

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import numpy as np
+from utils.read import read_fvecs
+from matplotlib import pyplot as plt
+
 
 if __name__ == "__main__":
-    pass
+    fv = read_fvecs("data/siftsmall_base.fvecs")
+
+    plt.title("Line by line plot of Dataset")
+
+    for line in fv:
+        plt.plot(line)
+
+    plt.show()
