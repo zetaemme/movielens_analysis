7d454677b89c622d2cefdcaa6d164823cca4ab66
zetaemme <mattia.zorzan@gmail.com>
2021-04-27 17:32:42 +0200

    Started PCA implementation, hoping that KNN will be as simple

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Big Data project. A.A. 2020/2021
+#
+# Mattia Zorzan - 2021 -
+# Email:  mattia(dot)zorzan_01(at)studenti(dot)univr(dot)it
+# ----------------------------------------------------------------------------------------------------------------------
 from utils.read import read_fvecs
-from matplotlib import pyplot as plt
 
+import numpy as np
 
 if __name__ == "__main__":
-    fv = read_fvecs("data/siftsmall_base.fvecs")
+	fv = read_fvecs("data/siftsmall_base.fvecs")
 
-    plt.title("Line by line plot of Dataset")
+	"""
+	Gets the dimensions from the dataset
+	
+	Variables:
+	N (int): Size of the dataset
+	dim (int): Starting dimensionality
+	"""
+	N, dim = fv.shape
 
-    for line in fv:
-        plt.plot(line)
+	"""
+	Dataset's mean
+	
+	Variables:
+	mean (np.float32)
+	"""
+	mean = np.mean(fv)
 
-    plt.show()
+	"""
+	Centering
+	
+	Variables:
+	h (np.ndarray of np.float64): 1xN Matrix used for centering purposes
+	centered (np.ndarray of np.float64): Centered dataset
+	"""
+	h = np.ones((N, 1))
+	centered = np.subtract(fv, (mean * h))
+
+	"""
+	Covariance matrix
+	
+	Variables:
+	cov (np.ndarray of np.float64)
+	"""
+	cov = 1/(N - 1) * np.dot(centered, centered.T)
